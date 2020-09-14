3cfc4bcbd59a7cc60848fd74e5afde42fdecb992
Donald Wu <yeukfei02@gmail.com>
2020-09-14 15:45:07 +0800

    refactor: 💡 fix job api

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -25,6 +25,8 @@ create table jobs (
     type varchar(255) NOT NULL,
     department varchar(255) NOT NULL,
     location text NOT NULL,
+    title varchar(255) NOT NULL,
+    description text NOT NULL,
     created_by timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_by timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
