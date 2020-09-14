7343b095e2f135af9d27f1135534d62bb912243f
Donald Wu <yeukfei02@gmail.com>
2020-09-14 14:30:37 +0800

    feat: 🎸 add job and country api

---
+++ src/service/jobs.ts
@@ -0,0 +1,29 @@
+import { PrismaClient, jobs } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const createJobs = async (type: string, department: string, location: string): Promise<void> => {
+  await prisma.jobs.create({
+    data: {
+      type: type,
+      department: department,
+      location: location,
+    },
+  });
+};
+
+export const getJobs = async (): Promise<jobs[]> => {
+  const jobsList = await prisma.jobs.findMany({});
+  return jobsList;
+};
+
+export const getJobsByFilter = async (type: string, department: string, location: string): Promise<jobs[]> => {
+  const jobsList = await prisma.jobs.findMany({
+    where: {
+      type: type,
+      department: department,
+      location: location,
+    },
+  });
+  return jobsList;
+};
