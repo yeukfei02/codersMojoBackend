a5d5249ec08dfa5d3ed693230607f39214983b26
Donald Wu <yeukfei02@gmail.com>
2020-09-19 22:31:32 +0800

    refactor: 💡 add deploy aws eb file

---
+++ deploy-aws-eb-staging.sh
@@ -0,0 +1,20 @@
+
+# create .elasticbeanstalk folder
+eb init
+
+# docker login
+aws --region ap-southeast-1 ecr get-login-password | docker login --username AWS --password-stdin https://151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging
+
+# run docker-compose
+docker-compose up -d --build
+
+# docker tag
+docker tag codersmojobackend_web:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest
+docker tag codersmojobackend_prisma-studio:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
+
+# docker push
+docker push 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest
+docker push 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
+
+# deploy command
+eb deploy --message="deploy new version"
