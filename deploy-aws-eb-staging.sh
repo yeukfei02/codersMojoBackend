7f2d351a169c11e160acc4de90e1a7ece2df887c
Donald Wu <yeukfei02@gmail.com>
2020-09-20 15:47:30 +0800

    refactor: 💡 fix file

--- deploy-aws-eb-staging.sh
+++ deploy-aws-eb-staging.sh
@@ -8,11 +8,11 @@ docker-compose up -d --build
 # docker login
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com
 
-# docker tag, docker push
+# docker tag, docker push (web)
 docker tag codersmojobackend_web:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest
 docker push 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest
 
-# docker tag, push
+# docker tag, push (prisma-studio)
 docker tag codersmojobackend_prisma-studio:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
 docker push 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
 
@@ -20,7 +20,8 @@ docker push 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backen
 docker-compose down
 
 # docker rmi images
-docker rmi codersmojobackend_web:latest codersmojobackend_prisma-studio:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
+docker rmi codersmojobackend_web:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-web:latest
+docker rmi codersmojobackend_prisma-studio:latest 151708502202.dkr.ecr.ap-southeast-1.amazonaws.com/coders-mojo-backend-staging-prisma-studio:latest
 
 # docker clear system, clear volume
 docker system prune
