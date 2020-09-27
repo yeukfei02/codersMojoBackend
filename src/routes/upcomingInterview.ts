68d7e5da8ce6157d065d4df1abc5ce45d70a6095
Donald Wu <yeukfei02@gmail.com>
2020-09-27 14:10:51 +0800

    feat: 🎸 add cancel upcoming interview api

--- src/routes/upcomingInterview.ts
+++ src/routes/upcomingInterview.ts
@@ -8,5 +8,10 @@ import { isUserLoggedIn } from '../middleware/middleware';
 
 router.post('/api/upcoming-interview', isUserLoggedIn, upcomingInterviewController.createUpcomingInterview);
 router.get('/api/upcoming-interview', isUserLoggedIn, upcomingInterviewController.getUpcomingInterview);
+router.put(
+  '/api/upcoming-interview/cancel-upcoming-interview/:upcomingInterviewId',
+  isUserLoggedIn,
+  upcomingInterviewController.cancelUpcomingInterview,
+);
 
 export default router;
